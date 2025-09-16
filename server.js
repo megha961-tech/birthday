0473d5ceb7162834420d738dc62497597a0df391
anamika961 <bera.anamika961@gmail.com>
2025-09-16 15:20:19 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 10000;
+
+// Serve static files (HTML, images, CSS)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Default route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🎂 Birthday app running at http://localhost:${PORT}`);
+});
